1cb2d027e9e23f548ed6abe0936c81e687b92887
SamChatfield <samchatfield97@gmail.com>
2017-03-05 18:17:27 +0000

    Tested ready for submit

--- Exercise3/evalobj.cpp
+++ Exercise3/evalobj.cpp
@@ -26,9 +26,7 @@ int lookup(struct env *en, string var) {
 }
 
 struct env *envadd(struct env *en, string var, int value) {
-    // struct env *current = malloc(sizeof(struct env));
     struct env *current = new struct env;
-    // strncpy(current->var, var, 8);
     current->var = var;
     current->value = value;
     current->next = en;
@@ -36,23 +34,19 @@ struct env *envadd(struct env *en, string var, int value) {
 }
 
 int Constant::eval(struct env *en) {
-    cout << "const";
     return this->n;
 }
 
 int Var::eval(struct env *en) {
-    cout << "var";
     return lookup(en, this->name);
 }
 
 int Let::eval(struct env *en) {
-    cout << "let";
     struct env *newen = envadd(en, this->bvar, this->bexp->eval(en));
     return this->body->eval(newen);
 }
 
 int OpApp::eval(struct env *en) {
-    cout << "opapp";
     switch (this->op) {
         case plusop:
             return evalplus(this->args, en);
@@ -64,7 +58,6 @@ int OpApp::eval(struct env *en) {
 int evalplus(ExpList *l, struct env *en) {
     int result = 0;
     while (l) {
-        // result += evalexpenv(l->head, en);
         result += l->head->eval(en);
         l = l->tail;
     }
@@ -74,7 +67,6 @@ int evalplus(ExpList *l, struct env *en) {
 int evalmult(ExpList *l, struct env *en) {
     int result = 1;
     while (l) {
-        // result *= evalexpenv(l->head, en);
         result *= l->head->eval(en);
         l = l->tail;
     }
