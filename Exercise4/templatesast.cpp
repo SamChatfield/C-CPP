9ddfedfe9e20157649b3b12efb15508b1a87e7db
SamChatfield <samchatfield97@gmail.com>
2017-03-14 19:44:48 +0000

    Ready for submit

--- Exercise4/templatesast.cpp
+++ Exercise4/templatesast.cpp
@@ -27,31 +27,26 @@ struct env<V> *envadd(struct env<V> *en, string var, V value) {
 
 template<typename V>
 V Constant<V>::eval(struct env<V> *en) {
-    // cout << "c=" << this->c << endl;
     return this->c;
 }
 
 template<typename V>
 V Var<V>::eval(struct env<V> *en) {
-    // cout << "var" << endl;
     return lookup(en, this->name);
 }
 
 template<typename V>
 V Let<V>::eval(struct env<V> *en) {
-    // cout << "let" << endl;
     struct env<V> *newen = envadd(en, this->bvar, this->bexp->eval(en));
     return this->body->eval(newen);
 }
 
 template<typename V>
 V OpApp<V>::eval(struct env<V> *en) {
-    // cout << "opapp" << endl;
     V result = this->ops.unit;
     ExpList<V> *l = this->args;
     while (l) {
         result = this->ops.binop(result, l->head->eval(en));
-        // cout << "res: " << result << endl;
         l = l->tail;
     }
     return result;
